7019b91b3a92e3e64675e6e2297c63b16b410050
SafaeElouatyq <safaeelouatyq@gmail.com>
2025-12-17 00:23:42 +0100

    some changes in contacts page(margin)

--- lib/pages/contacts_page.dart
+++ lib/pages/contacts_page.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import '../models/contact.dart';
-
 
 class ContactsPage extends StatefulWidget {
   const ContactsPage({super.key});
@@ -11,24 +9,26 @@ class ContactsPage extends StatefulWidget {
 }
 
 class _ContactsPageState extends State<ContactsPage> {
-  List<Contact> contacts=[
-    Contact(name:"safae",phone:"1234567890"),
+  List<Contact> contacts = [
+    Contact(name: "Safae", phone: "1234567890"),
+    Contact(name: "mustapha", phone: "9876543210"),
+
   ];
 
-  List<Contact> filteredContacts =[];
+  List<Contact> filteredContacts = [];
 
   final TextEditingController searchController = TextEditingController();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     filteredContacts = contacts;
   }
 
-  void filterContacts(String query){
-    final filtered =contacts.where((contact){
+  void filterContacts(String query) {
+    final filtered = contacts.where((contact) {
       final nameLower = contact.name.toLowerCase();
-      final phoneLower= contact.phone.toLowerCase();
+      final phoneLower = contact.phone.toLowerCase();
       final queryLower = query.toLowerCase();
 
       return nameLower.contains(queryLower) || phoneLower.contains(queryLower);
@@ -37,44 +37,62 @@ class _ContactsPageState extends State<ContactsPage> {
     setState(() {
       filteredContacts = filtered;
     });
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contacts ${contacts.length}"),
+        title: Text("Contacts (${contacts.length})"),
         centerTitle: true,
       ),
       body: Column(
         children: [
-          TextField(
-            controller:searchController,
-            decoration: InputDecoration(
-              labelText: "Search by name or phone",
-              border: OutlineInputBorder(),
+
+          Container(
+            width: 300,
+            height: 50,
+            margin: EdgeInsets.fromLTRB(10, 30, 10, 20), // left, top, right, bottom
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                labelText: "Search by name or phone",
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    searchController.clear();
+                    filterContacts('');
+                  },
+                ),
+              ),
+              onChanged: filterContacts,
             ),
-            onChanged: filterContacts,
           ),
-          SizedBox(height: 10),
-          Expanded(child: ListView.builder(itemCount: filteredContacts.length,itemBuilder: (context,index){
-            final contact = filteredContacts[index];
-            return ListTile(
-              title: Text(contact.name),
-              subtitle: Text(contact.phone),
-            );
-          },),)
+
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredContacts.length,
+              itemBuilder: (context, index) {
+                final contact = filteredContacts[index];
+                return Column(
+                  children: [
+                    ListTile(
+                      leading:CircleAvatar(
+                        child: Text(contact.name[0].toUpperCase()),
+                      ),
+                      title: Text(contact.name),
+                      subtitle: Text(contact.phone),
+                    ),
+                    Divider(),
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
-
     );
   }
-
-
-
-
-
 }
